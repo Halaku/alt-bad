bc8632932ce8e6b6c5c181224b217c52d5ddbebc
Alexey Solovyov <halakul@live.ru>
2016-02-12 16:22:13 +0300

    js optimization

---
+++ app/js/input-tooltip.js
@@ -0,0 +1,83 @@
+(function() {
+    var submit = $('.tooltip-submit-button'),
+        reset = $('.tooltip-reset-button'),
+        closeWindow = $('.tooltip-close-button'),
+        mask = $('.tooltip-mask'),
+        popupOpen = $('.tooltip-popup-open'),
+        popupWrapper = $('.tooltip-popup-wrapper'),
+        popupWindow = $('.tooltip-popoup-window'),
+        popupSuccess = $('.tooltip-popup-success'),
+        popupError = $('.tooltip-popup-error'),
+        inputs = $('.tooltip-input'),
+        inputFile = $('.tooltip-file'),
+        inputFileFake = $('.tooltip-file-fake'),
+        tooltip = $('.tooltip, .tooltip-right'),
+        noTooltip = false;
+
+
+    function inputCheck() {
+        $(tooltip.selector).remove();
+        $(inputs).each(function(index, elem) {
+            var tooltipSide = $(elem).attr('data-pos'),
+                errorText = $(elem).attr('data-error');
+            if ($.trim($(elem).val())) {
+                $(elem).removeClass('input-error-highlight');
+
+            } else {
+                $(elem).addClass('input-error-highlight');
+                if (tooltipSide == 'left') {
+                    $(elem).parent().append('<div class="tooltip">' + errorText + '</div>');
+                } else {
+                    $(elem).parent().append('<div class="tooltip-right">' + errorText + '</div>');
+                }
+            }
+        });
+        if ($(inputs).hasClass('input-error-highlight')) {
+            popupError.show(200);
+        } else {
+            popupError.hide(200);
+            return true;
+        }
+    }
+
+    function resetInput() {
+        $(inputs)
+            .off()
+            .val('')
+            .removeClass('input-error-highlight');
+        $(inputFile).off();
+        popupError.hide();
+        popupSuccess.hide();
+        $(tooltip.selector).remove();
+    }
+
+    popupOpen.click(function() {
+        resetInput();
+        popupWrapper.show();
+        popupWindow.show();
+        inputFile.change(function() {
+            $(inputFileFake).val($(inputFile).val());
+        });
+    });
+
+    submit.click(function(e) {
+        e.preventDefault();
+        noTooltip = inputCheck();
+        $(inputs).keyup(inputCheck);
+        $(inputFile).change(inputCheck);
+        if (noTooltip) {
+            popupWindow.hide(200);
+            popupSuccess.show(200);
+        }
+    });
+
+    reset.click(resetInput);
+
+    closeWindow.click(function() {
+        $(popupWrapper).hide();
+    });
+
+    mask.click(function() {
+        $(popupWrapper).hide();
+    });
+})();
